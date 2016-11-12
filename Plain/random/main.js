ab9915d49fc0fbeb09d8d45a0dd5f561e3c70997
Alex Munoz <alex@napend.com>
2016-11-12 12:35:49 -0500

    Folders and Vue

---
+++ Plain/random/main.js
@@ -0,0 +1,6 @@
+var name = '';
+var age = '';
+var year = '';
+var div = document.getElementById('div1');
+div.innerHTML = name + " " + age + " " + year
+var indiv = div.innerHTML = name + " " + age + " " + year
